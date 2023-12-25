3c588e90415dad94cfaf6c29babf0c58c5f1eef5
onur-dursun <dursunon@itu.edu.tr>
2023-12-26 02:29:50 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const { QMainWindow, QWidget, FlexLayout, QLabel } = require("@nodegui/nodegui");
-const sound = require("sound-play");
+//const sound = require("sound-play");
 const path = require('path');
 const url = require('url');
+
+const fs = require('fs')
+const symphonia = require('@tropicbliss/symphonia')
 
 const rootView = new QWidget();
 const rootLayout = new FlexLayout();
@@ -29,6 +32,15 @@ win.show();
 
 global.win = win; // prevent's gc of win
 
-const filePath = path.join(__dirname, "assets\\stack_alarm.mp3");
-console.log(filePath);
-sound.play(filePath);
+// const filePath = path.join(__dirname, "assets\\stack_alarm.mp3");
+// console.log(filePath);
+// sound.play(filePath);
+
+
+try{
+  const buf = fs.readFileSync('./assets/stack_alarm.mp3') // Gets a Buffer
+  symphonia.playFromBuf(buf, { speed: 1.0, volume: 1.0, isBlocking: false }) // The option object is optional. The speed and volume is both set to 1.0 and `isBlocking` is set to `true` by default.
+}
+catch{
+
+}
